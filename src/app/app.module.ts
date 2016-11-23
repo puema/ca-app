8badd04925b47c109a87f9e18ce4b63b82e98a79
Marcel <marcelpuetz@web.de>
2016-11-23 11:41:49 +0100

    feat: add contacts component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/contacts/contacts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.less']
+})
+export class ContactsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
