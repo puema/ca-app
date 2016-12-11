263d68e2563d9f40f0fbe57be747b6c74749f429
Marcel <marcelpuetz@web.de>
2016-12-11 16:01:35 +0100

    refactor: flex styles

--- src/app/contact-details/contact-details.component.ts
+++ src/app/contact-details/contact-details.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface IContactDetails {
+  firstName : string,
+  lastName : string,
+  mobile : string[],
+  mail : string[],
+  address : string[],
+  birthday : string
+}
+
 @Component({
   selector: 'contact-details',
   templateUrl: './contact-details.component.html',
@@ -7,18 +16,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactDetailsComponent implements OnInit {
 
-  contact : {} = {
-    firstname: "Harvey",
-    lastname: "Spectre",
+  contact : IContactDetails = {
+    firstName: 'Harvey',
+    lastName: 'Spectre',
     mobile: [
-      "+49123456789",
-      "+432345678901"
-    ]
+      '+49123456789',
+      '+432345678901'
+    ],
+    mail: [
+      'harvey.spectre@psl.com'
+    ],
+    address: [
+      '601 E 54th St',
+      'New York City',
+      'New York',
+      '10000'
+    ],
+    birthday: '19.11.1983'
   };
 
-  constructor() { }
+  constructor () {
+  }
 
-  ngOnInit() {
+  ngOnInit () {
   }
 
 }
