2c1ff369d971d1e396d64f377601f27aa8284280
Marcel <marcelpuetz@web.de>
2016-12-14 10:29:12 +0100

    refactor: merge contact-list-header and contact-list-body component into a single component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { HttpInterceptorModule } from 'angular2-http-interceptor/lib/index';
-import { TokenInterceptor } from './data/token-interceptor/token-interceptor';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { HttpInterceptorModule } from "angular2-http-interceptor/lib/index";
+import { TokenInterceptor } from "./data/token-interceptor/token-interceptor";
 import { MaterialModule } from "@angular/material";
-
-import { AppComponent } from './app.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { HeaderComponent } from './header/header.component';
-import { ContactsListComponent } from './contacts-list/contacts-list.component';
-import { ContactsListHeaderComponent } from './contacts-list-header/contacts-list-header.component';
-import { ContactsListBodyComponent } from './contacts-list-body/contacts-list-body.component';
-import { ContactDetailsComponent } from './contact-details/contact-details.component';
+import { AppComponent } from "./app.component";
+import { ContactsComponent } from "./contacts/contacts.component";
+import { HeaderComponent } from "./header/header.component";
+import { ContactsListComponent } from "./contacts-list/contacts-list.component";
+import { ContactDetailsComponent } from "./contact-details/contact-details.component";
 
 @NgModule({
   declarations: [
@@ -21,8 +17,6 @@ import { ContactDetailsComponent } from './contact-details/contact-details.compo
     ContactsComponent,
     HeaderComponent,
     ContactsListComponent,
-    ContactsListHeaderComponent,
-    ContactsListBodyComponent,
     ContactDetailsComponent,
   ],
   imports: [
