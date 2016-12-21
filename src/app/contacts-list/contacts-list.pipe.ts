fc5849c2ab8ce95bf24f6aa5f8775793ba3db1f9
Marcel <marcelpuetz@web.de>
2016-12-21 12:32:55 +0100

    feat: add capability to search in lower case

--- src/app/contacts-list/contacts-list.pipe.ts
+++ src/app/contacts-list/contacts-list.pipe.ts
@@ -6,12 +6,16 @@ import { ContactsListItem } from "./contacts-list.component";
 })
 export class ContactsListPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
+  transform (value : any, args? : any) : any {
     if (!Array.isArray(value) || !(typeof args === 'string')) {
       return;
     }
+    const input : string = args.toLowerCase();
     const items : ContactsListItem[] = <ContactsListItem[]> value;
-    return items.filter(item => item.firstname.indexOf(args) !== -1 || item.lastname.indexOf(args) !== -1);
+    return items.filter(item =>
+      item.firstname.toLowerCase().indexOf(input) !== -1 ||
+      item.lastname.toLowerCase().indexOf(input) !== -1
+    );
   }
 
 }
