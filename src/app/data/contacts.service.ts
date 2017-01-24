d60a148180ecb15d64fd77059152e531a7aa3c17
Marcel <marcelpuetz@web.de>
2017-01-24 21:53:10 +0100

    fix: edit scrolling

--- src/app/data/contacts.service.ts
+++ src/app/data/contacts.service.ts
@@ -15,14 +15,14 @@ export class ContactsService {
   }
 
   public getContacts () : Observable<ContactDto[]> {
-    return this.endpoint.contactsGetAll();
+    // return this.endpoint.contactsGetAll();
 
-    // return Observable.create(observer => {
-    //   // Yield a single value and complete
-    //   observer.next(require('../../../contactMock.json'));
-    //   observer.complete();
-    //
-    // });
+    return Observable.create(observer => {
+      // Yield a single value and complete
+      observer.next(require('../../../contactMock.json'));
+      observer.complete();
+
+    });
   }
 
 }
